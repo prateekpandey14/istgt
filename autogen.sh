3aa46790e00aa4f54bada4e7cf101a45d20ade63
prateekpandey14 <prateekpandey14@gmail.com>
2020-03-12 13:43:20 +0530

    change libcstor branch
    Signed-off-by: prateekpandey14 <prateekpandey14@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@
 source ./fetch-libcstor-branch.sh
 
 echo "Using libcstor branch - ${LIBCSTOR_BUILD_BRANCH}"
-echo $(wget -O /tmp/zrepl_prot.h https://raw.githubusercontent.com/openebs/libcstor/${LIBCSTOR_BUILD_BRANCH}/include/zrepl_prot.h)
+echo $(wget -O /tmp/zrepl_prot.h https://raw.githubusercontent.com/openebs/libcstor/master/include/zrepl_prot.h)
 
 autoreconf -fiv
 rm -Rf autom4te.cache
